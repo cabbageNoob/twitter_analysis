38d64c2a1f4ec25c1352df70494ecbce743e3f7f
cjh <492795090@qq.com>
2020-01-05 17:51:09 +0800

    add socialnet to flask app

--- app/socialnet/views.py
+++ app/socialnet/views.py
@@ -1,10 +1,28 @@
+'''
+@Descripttion: 
+@version: 
+@Author: Da Chuang
+@Date: 2020-01-05 16:15:39
+@LastEditors  : Da Chuang
+@LastEditTime : 2020-01-05 17:30:51
+'''
 from . import socialnet
 from flask import render_template
+
+import sys, os, json
+sys.path.insert(0, os.getcwd())
+from utils import common_util
+import config
 
 
 @socialnet.route('/socialnetindex')
 def socialnetindex():
-    return render_template('socialnet/socialnetindex.html')
+    data = dict()
+    nodes = common_util.readjson(config.TWITTER_NODES_FILE)
+    edges = common_util.readjson(config.TWINT_EDGES_FILE)
+    data['nodes'] = nodes
+    data['edges'] = edges
+    return render_template('socialnet/socialnetindex.html', data=json.dumps(data))
 
 @socialnet.route('/socialman',methods=['GET', 'POST'])
 def manQuery():
--- flasky.py
+++ flasky.py
@@ -1,7 +1,15 @@
+'''
+@Descripttion: 
+@version: 
+@Author: Da Chuang
+@Date: 2020-01-05 16:15:39
+@LastEditors  : Da Chuang
+@LastEditTime : 2020-01-05 17:17:58
+'''
 import os
 from app import create_app
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__=='__main__':
-    app.run(debug=True,port=8080)
+    app.run(host='0.0.0.0',debug=True,port=8001)
